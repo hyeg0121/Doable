97099eda7882d069dd1211780fbcc2e4c669eef9
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-13 19:53:37 +0900

    feat: 유저 아이디로 유저가 가입된 그룹 찾기

--- routes/users.js
+++ routes/users.js
@@ -44,5 +44,25 @@ router.get('/:user_no', (req, res) => {
     )
 });
 
+router.get('/:user_no/groups', (req, res) => {
+    const user_no = req.params.user_no;
+
+    pool.query(
+        `SELECT todo_group.*
+        FROM membership
+        INNER JOIN todo_group ON membership.group_no = todo_group.group_no
+        WHERE membership.user_no = ?`,
+        [user_no],
+        (err, results) => {
+            if (err) {
+                console.error('검색 오류: ', err);
+                res.status(500).json({message: '데이터를 찾을 수 없음'});
+            }else {
+                console.log('검색 성공')
+                res.status(200).json(results);
+            }
+        });
+});
+
 
 module.exports = router;
