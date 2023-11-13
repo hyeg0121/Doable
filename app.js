f97617105531bdb8703aab04fbb328481d32bc23
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-13 22:40:18 +0900

    feat: todo 추가 api

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 const groupsRouter = require('./routes/groups');
 const authRouter = require('./routes/auth');
-// const todosRouter = require('./routes/todos');
+const todosRouter = require('./routes/todos');
 const categoriesRouter = require('./routes/categories');
 
 app.use('/users', usersRouter);
 app.use('/groups', groupsRouter);
 app.use('', authRouter);
-// app.use('/todos', todosRouter);
+app.use('/todos', todosRouter);
 app.use('/categories', categoriesRouter);
 
 
