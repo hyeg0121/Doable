d2c05cfe8b2ef34637e42406b2b28f1dddc3edaa
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-13 22:46:44 +0900

    feat: 카테고리 생성, 조회

---
+++ routes/categories.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const pool = require('../db/db');
+const bodyParser = require('body-parser');
+const corsMiddleware = require('../middlewares/cors');
+const session = require('../middlewares/session');
+const router = express.Router();
+
+router.use(bodyParser.json());
+router.use(corsMiddleware);
+session.configureSession(router);
+
+router.get('/', (req, res) => {
+    pool.query(
+        'SELECT * FROM category',
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(400).json(err);
+            } else if (results.length === 0) {
+                res.status(404).json({message: '데이터가 존재하지 않습니다.'})
+            } else {
+                res.status(400).json(results);
+            }
+        }
+    )
+});
+
+router.post('/', (req, res) => {
+    const params = [
+        req.body.user_no,
+        req.body.category_name,
+        req.body.category_color
+    ];
+
+    pool.query(
+        `INSERT INTO category (user_no, category_name, category_color) VALUES (?, ?, ?)`,
+        params,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                res.status(400).json({error: '카테고리 추가하는데 실패했습니다.'});
+            } else {
+                res.status(201).json({message: '카테고리가 성공적으로 추가되었습니다.'})
+            }
+        }
+    );
+
+});
+
+
+module.exports = router;
