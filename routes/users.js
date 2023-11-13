78522295b96ed892672da22e2756766c484ab521
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-13 14:40:28 +0900

    feat: 모든 유저 조회, 아이디로 검색된 유저 조회

---
+++ routes/users.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const pool = require('../db/db');
+const bodyParser = require('body-parser');
+const corsMiddleware = require('../middlewares/cors');
+const session = require('../middlewares/session');
+const router = express.Router();
+
+router.use(bodyParser.json());
+router.use(corsMiddleware);
+session.configureSession(router);
+
+router.get('/', (req, res) => {
+    pool.query(
+        'SELECT * FROM user',
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(400).json(err);
+            } else if (results.length === 0) {
+                res.status(404).json({ message: '데이터가 존재하지 않습니다.' })
+            } else {
+                res.status(400).json(results);
+            }
+        }
+    )
+});
+
+router.get('/:user_no', (req, res) => {
+    const user_no = req.params.user_no;
+
+    pool.query(
+        'SELECT * FROM user WHERE user_no = ?',
+        [user_no],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(400).json(err);
+            } else if (results.length === 0) {
+                res.status(404).json({ message: '데이터가 존재하지 않습니다.' })
+            } else {
+                res.status(400).json(results[0]);
+            }
+        }
+    )
+});
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
