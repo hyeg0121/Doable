3541d6b09e92ed6d3506601dd55151bfee27cff5
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-13 20:11:40 +0900

    feat: 그룹 만들면 자동으로 그룹 가입, 그룹 아이디로 조회하는 api

--- routes/groups.js
+++ routes/groups.js
@@ -23,12 +23,24 @@ router.post('/', (req, res) => {
         `INSERT INTO todo_group (group_name, group_desc, group_search, group_todo, creator_no, group_unit)
         VALUES (?, ?, ?, ?, ?, ?)`,
         params,
-        (err, rows) => {
+        (err, result) => {
             if (err) {
                 console.error(err);
                 res.status(400).json({error: '그룹을 추가할 수 없습니다.'});
             } else {
-                res.status(201).json({message: '그룹이 성공적으로 추가되었습니다.'});
+                pool.query(
+                    `INSERT INTO membership (user_no, group_no, created_at) VALUES (? ,?, now())`,
+                    [params[4], result.insertId],
+                    (err, result) => {
+                        if (err) {
+                            console.error('멤버 추가 오류:', err);
+                            res.status(400).json({error: '멤버 추가 오류'});
+                        } else {
+                            console.log('멤버 추가 성공');
+                            res.status(201).json({message: '그룹이 성공적으로 추가되었습니다.'});
+                        }
+                    }
+                )
             }
         }
     )
@@ -50,5 +62,23 @@ router.get('/', (req, res) => {
     );
 });
 
+router.get('/:group_no', (req, res) => {
+    const group_no = req.params.group_no;
+
+    pool.query(
+        `SELECT * FROM todo_group WHERE group_no = ?`,
+        [group_no],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(400).json(err);
+            } else if (results.length === 0) {
+                res.status(404).json({message: '데이터가 존재하지 않습니다.'})
+            } else {
+                res.status(200).json(results[0]);
+            }
+        }
+    )
+});
 
 module.exports = router;
