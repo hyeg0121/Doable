d181c5d5ff16a2ed0a89814dc4ea751bf8dd77d1
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-16 01:35:22 +0900

    feat: json 삭제

--- db/db.js
+++ db/db.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const mysql = require('mysql2');
-const config = require('../config/config.json')
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
