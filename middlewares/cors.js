c8a246bfd2d2e035e43ca229412b3a6b06390cef
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-17 14:32:00 +0900

    feat: cors 허용

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,3 +1,19 @@
 const cors = require('cors');
 
+const whitelist = ['https://do-able.netlify.app/'];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
+
 module.exports = cors();
