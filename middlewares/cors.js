6febd8330169cdc5bb1b53131b230279fda9fa4b
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-17 19:10:49 +0900

    feat: 콜스 설정 변경

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,19 +1,4 @@
 const cors = require('cors');
 
-const whitelist = ['https://do-able.netlify.app/'];
-
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204,
-};
-
 
 module.exports = cors();
